104119a5f760204347f27d9540e6f8952ff496ae
Piyush Sinha <piyushsinha0526@gmail.com>
2023-03-15 12:17:41 +0530

    add: lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,50 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar, PrivateRoute } from "./components";
 import { AuthProvider } from "./context/authContext";
-import { Details, Home, Movie, Tv, Library, Search } from "./pages";
 import Auth from "./pages/Auth";
 import ScrollPageChange from "./utils/ScrollPageChange";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+const Home = lazy(() => import("./pages/Home"));
+const Details = lazy(() => import("./pages/Details/Details"));
+const Movie = lazy(() => import("./pages/Movie"));
+const Tv = lazy(() => import("./pages/Tv"));
+const Library = lazy(() => import("./pages/Library"));
+const Search = lazy(() => import("./pages/Search"));
 
 function App() {
   return (
     <div className="App ">
       <AuthProvider>
-            <ScrollPageChange />
+        <ScrollPageChange />
         <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/movie" element={<Movie />} />
-          <Route path="/tv" element={<Tv />} />
-          <Route element={<PrivateRoute />}>
-            <Route element={<Library />} path="/library" />
-          </Route>
-          <Route path="/auth" element={<Auth />} />
-          <Route path=":type/details/:id" element={<Details />} />
-          <Route path="search/:query" element={<Search />} />
-          <Route path="search/:query/:type/details/:id" element={<Details />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="bg-gray-900 w-screen h-screen flex justify-center items-center">
+              <span className="animate-spin ">
+                <AiOutlineLoading3Quarters size={50} />
+              </span>
+            </div>
+          }
+        >
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/movie" element={<Movie />} />
+            <Route path="/tv" element={<Tv />} />
+            <Route element={<PrivateRoute />}>
+              <Route element={<Library />} path="/library" />
+            </Route>
+            <Route path="/auth" element={<Auth />} />
+            <Route path=":type/details/:id" element={<Details />} />
+            <Route path="search/:query" element={<Search />} />
+            <Route
+              path="search/:query/:type/details/:id"
+              element={<Details />}
+            />
+          </Routes>
+        </Suspense>
       </AuthProvider>
     </div>
   );
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Home } from "./Home";
-export { default as Library } from "./Library";
-export { default as Movie } from "./Movie";
-export { default as Details } from "./Details/Details";
-export { default as Tv } from "./Tv";
-export { default as Search } from "./Search";
